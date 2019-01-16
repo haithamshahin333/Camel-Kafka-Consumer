461378e4222b89cb27b31da56d3441fba57e347f
Haitham Shahin <hshahin@redhat.com>
2019-01-16 12:09:46 -0500

    first commit

---
+++ src/main/java/com/domainevent/sample/camelConsumer/CamelConsumerApplication.java
@@ -0,0 +1,14 @@
+package com.domainevent.sample.camelConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelConsumerApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/domainevent/sample/camelConsumer/CamelConsumerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.domainevent.sample.camelConsumer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CamelConsumerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
